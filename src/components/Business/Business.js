c2f8a1a52ad348d8203c818fb01e667cf49687ba
grobmo <webmaster@grobmo.de>
2020-05-28 22:43:27 +0200

    startup

---
+++ src/components/Business/Business.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Business.css';
+
+
+var business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+
+class Business extends React.Component {
+    render () {
+        <div className="Business">
+        <div className="image-container">
+            <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+        </div>
+        <h2>{business.this.name}</h2>
+        <div className="Business-information">
+            <div className="Business-address">
+            <p>{business.this.address}</p>
+            <p>{business.this.city}</p>
+            <p>{business.this.state + " " + business.this.zipCode}</p>
+            </div>
+            <div className="Business-reviews">
+            <h3>{business.this.category}</h3>
+            <h3 className="rating">{business.this.rating} stars}</h3>
+            <p>{business.this.reviewCount} reviews</p>
+            </div>
+        </div>
+        </div>
+    }
+}
+
+export default Business;
