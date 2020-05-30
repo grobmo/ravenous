7bb59038415ea607acdbea31fec10b764851d137
grobmo <webmaster@grobmo.de>
2020-05-30 22:45:05 +0200

    businesslist

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+
+class BusinessList extends React.Component {
+    render () {
+        <div className="BusinessList">
+            <!-- Add Business Component here --> 
+            <!-- Add Business Component here -->
+            <!-- Add Business Component here -->
+            <!-- Add Business Component here -->
+            <!-- Add Business Component here -->
+            <!-- Add Business Component here -->
+        </div>
+    }
+}
